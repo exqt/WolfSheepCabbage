852b88ae96996e9de82e29efeeb3857db24e89f7
exqt <exqt_@naver.com>
2019-02-19 00:50:31 +0900

    Add some levels

---
+++ level/C1.lua
@@ -0,0 +1,10 @@
+return {
+    name = "WOLF, SHEEP AND CABBAGE 1",
+    data = {
+        ".---.---.",
+        "--s-.--S-",
+        "-c--=-CP-",
+        "--w-.--W-",
+        ".---.---.",
+    }
+}
